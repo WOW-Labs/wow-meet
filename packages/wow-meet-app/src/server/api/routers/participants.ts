b2e1875fbbc1f3589a451e5b3eb4960b8a7c3705
Bong <qhdgkdbs@gmail.com>
2023-07-14 21:03:11 +0900

    ✨updateMeetingParticipationSchedule 라우터 작업
    - 라우터 전반에 걸친 에러처리가 필요함

--- packages/wow-meet-app/src/server/api/routers/participants.ts
+++ packages/wow-meet-app/src/server/api/routers/participants.ts
@@ -36,4 +36,41 @@ export const paticipantsRouter = createTRPCRouter({
         return { success: false };
       }
     }),
+  updateMeetingParticipationSchedule: publicProcedure
+    .input(
+      z.object({
+        meetingId: z.string(),
+        user: z.object({ name: z.string(), password: z.string().optional() }),
+        schelduleList: z
+          .array(z.object({ weight: z.number(), date: z.date() }))
+          .optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const userInfoId = await ctx.prisma.participants.findFirst({
+          where: {
+            name: input.user.name,
+            password: input.user.password || "",
+            meetingId: input.meetingId,
+          },
+          select: { id: true },
+        });
+
+        if (!userInfoId) {
+          // 유저 정보가 일치하지 않음
+          throw new Error("유효한 사용자 정보가 없습니다.");
+        }
+
+        await ctx.prisma.participants.update({
+          where: { id: userInfoId.id },
+          data: { schelduleList: JSON.stringify(input?.schelduleList || []) },
+        });
+
+        return { success: true };
+      } catch (err) {
+        console.log(err);
+        return { success: false };
+      }
+    }),
 });
