7c0019e6a934cfb50be2e7ab1331b090d069ad84
Jun99uu_Lee <igun0423@naver.com>
2023-07-30 18:52:04 +0900

    💄 체크박스 컴포넌트 추가

---
+++ packages/wow-meet-app/src/components/Vote/VoteList/index.tsx
@@ -0,0 +1,33 @@
+import styled from "@emotion/styled";
+import { dummyVoteData } from "~/assets/dummydata";
+import VoteItem from "~/components/Vote/VoteItem";
+import { useVote } from "~/hooks/useVote";
+
+//TODO 내 정보 가져오기
+const MY_NAME = "중규리";
+
+const VoteList = () => {
+  const { voteList, vote, isVoted } = useVote(dummyVoteData.list);
+
+  return (
+    <ListWrapper>
+      {dummyVoteData.list.map((listItem) => (
+        <VoteItem
+          {...listItem}
+          checked={isVoted(listItem.item, MY_NAME)}
+          onChange={() => vote(listItem.item, MY_NAME)}
+        />
+      ))}
+    </ListWrapper>
+  );
+};
+
+const ListWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+
+export default VoteList;
--- packages/wow-meet-app/src/pages/meeting/[mid]/vote.tsx
+++ packages/wow-meet-app/src/pages/meeting/[mid]/vote.tsx
@@ -2,12 +2,14 @@ import { dummyVoteData } from "~/assets/dummydata";
 import { Header } from "~/components/Bar";
 import Frame, { frameStyle } from "~/components/Frame";
 import { VoteBanner } from "~/components/Vote";
+import VoteList from "~/components/Vote/VoteList";
 
 const Vote = () => {
   return (
     <Frame css={frameStyle}>
       <Header title={dummyVoteData.title} />
       <VoteBanner content={"지금 와우밋 투표에 참여해보세요!"} />
+      <VoteList />
     </Frame>
   );
 };
