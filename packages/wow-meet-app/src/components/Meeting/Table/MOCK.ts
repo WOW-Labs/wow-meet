641c8878ed8b1f9a8e59ec748c074d5d6005a07a
Bong <qhdgkdbs@gmail.com>
2023-08-01 20:49:14 +0900

    🤡 타임테이블에서 사용될 목업 데이터 추가

---
+++ packages/wow-meet-app/src/components/Meeting/Table/MOCK.ts
@@ -0,0 +1,23 @@
+export interface ParticipantSchedule {
+  name: string;
+  scheduleList: { date: string; weight: number }[];
+}
+
+export const MOCK_UP_SELECTED_LIST = [
+  {
+    name: "봉승우",
+    scheduleList: [
+      { date: "월-09:00", weight: 1 },
+      { date: "월-19:00", weight: 1 },
+    ],
+  },
+  {
+    name: "이준규",
+    scheduleList: [
+      { date: "월-09:00", weight: 1 },
+      { date: "월-12:00", weight: 2 },
+    ],
+  },
+];
+
+export const MOCK_UP_DAY_LIST = ["월", "화", "수", "목", "금", "토", "일"];
