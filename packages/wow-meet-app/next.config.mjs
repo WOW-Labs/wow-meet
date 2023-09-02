0bd5f127f43178ffbb94759b1e6e1050c7926077
Bong <qhdgkdbs@gmail.com>
2023-09-02 10:59:49 +0900

    asdf

--- packages/wow-meet-app/next.config.mjs
+++ packages/wow-meet-app/next.config.mjs
@@ -22,6 +22,13 @@ const config = {
   compiler: {
     emotion: true,
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default config;
